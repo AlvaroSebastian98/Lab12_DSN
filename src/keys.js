db24b19149d82780c5e3612bdaeef3b0681adba9
Alvaro <alvaro.sebastian98@gmail.com>
2019-11-08 18:39:35 -0500

    first commit

--- src/keys.js
+++ src/keys.js
@@ -2,15 +2,6 @@ module.exports = {
 
     // database: {
     //     connectionLimit: 10,
-    //     host: 'us-cdbr-iron-east-05.cleardb.net',
-    //     user: 'b0655ea373feff',
-    //     password: '738467e4',
-    //     database: 'heroku_5e9a53f250f17f8',
-    //     // port: '8889'
-    // }
-
-    // database: {
-    //     connectionLimit: 10,
     //     host: process.env.MYSQL_HOST || 'localhost',
     //     user: 'root',
     //     password: 'root',
--- src/routes/links.js
+++ src/routes/links.js
@@ -1,35 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-// // AWS
-// const AWS = require('aws-sdk');
-// const fs = require('fs');
-// const path = require('path');
-// const multer = require('multer')
-// const multerS3 = require('multer-s3')
-
-//configuring the AWS environment
-// AWS.config.update({
-//     accessKeyId: "AKIAJSAE5MIBTH2RLHAA",
-//     secretAccessKey: "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"
-// });
-
-// var s3 = new AWS.S3();
-
 const pool = require('../database');
 const { isLoggedIn } = require('../lib/auth');
-
-// let upload = multer({
-//     storage: multerS3({
-//         s3: s3,
-//         bucket: 'bucket-app-01',
-//         key: function (req, file, cb) {
-//             console.log(file);
-//             // cb(null, file.originalname); //use Date.now() for unique file keys
-//             cb(null, "folder/"+Date.now()+"_"+path.basename(file.originalname))
-//         }
-//     })
-// });
 
 router.get('/add', (req, res) => {
     res.render('links/add');
@@ -55,24 +28,6 @@ router.post('/add', async (req, res) => {
     res.redirect('/links');
 });
 
-// router.post('/add', upload.array('imagen',1), async (req, res, next) => {
-//     const { nombre, precio, categoria, descripcion } = req.body; 
-//     let key = req.files[0].location
-//     console.log(key);
-//     const newLink = {
-//         nombre,
-//         precio,
-//         categoria,
-//         descripcion,
-//         imagen: key,
-//         user_id: req.user.id
-//     };
-//     await pool.query('INSERT INTO links set ?', [newLink]);
-//     req.flash('success', 'Producto registrado satisfactoriamente');
-//     res.redirect('/links');
-//     // res.send("Uploaded!");
-// });
-
 router.get('/', isLoggedIn, async (req, res) => {
     const links = await pool.query('SELECT * FROM contacts WHERE user_id = ?', [req.user.id]);
     res.render('links/list', { links });
@@ -91,27 +46,6 @@ router.get('/edit/:id', async (req, res) => {
     console.log(links);
     res.render('links/edit', {link: links[0]});
 });
-
-// router.post('/edit/:id', upload.array('imagen',1), async (req, res) => {
-//     const { id } = req.params;
-//     const { nombre, precio, categoria, descripcion, imagenUrl, imagen} = req.body; 
-//     let key = ''
-//     if(req.files.length == 0 ) {
-//         key = imagenUrl
-//     } else {        
-//         key = req.files[0].location
-//     } 
-//     const newLink = {
-//         nombre,
-//         precio,
-//         categoria,
-//         descripcion,
-//         imagen: key
-//     };
-//     await pool.query('UPDATE links set ? WHERE id = ?', [newLink, id]);
-//     req.flash('success', 'Producto actualizado satisfactoriamente');
-//     res.redirect('/links');
-// });
 
 router.post('/edit/:id', async (req, res) => {
     const { id } = req.params;
